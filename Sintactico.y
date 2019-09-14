6025b7c5eb89588fbfec688e3de2f66f88067dd0
Reflejo <tocinonaro_juan@yahoo.com.ar>
2019-09-14 13:10:49 -0300

    Entrega sin tree

---
+++ Sintactico.y
@@ -0,0 +1,187 @@
+%{
+#include <stdio.h>
+#include <stdlib.h>
+#include "symbol.h"
+#include <string.h>
+
+#define YYDEBUG 1
+extern int yylex();
+extern int yyparse();
+extern yylineno;
+extern FILE* yyin;
+
+char* seen[200];
+int seenIndex = 0;
+%}
+
+%token ID CTE_INT CTE_STRING CTE_REAL
+%token ASIG OP_SUMA OP_RESTA OP_MULT OP_DIV
+%token MENOR MAYOR IGUAL DISTINTO MENOR_IGUAL MAYOR_IGUAL
+%token CONST
+%token FILTER
+%token REPEAT UNTIL
+%token IF ELSE ENDIF
+%token P_A P_C C_A C_C
+%token GUION_BAJO
+%token COMA PUNTO_COMA DOS_PUNTOS
+%token AND OR NOT
+%token INT FLOAT STRING 
+%token VAR ENDVAR
+%token PRINT READ
+%left OP_SUMA
+%left OP_RESTA
+%left OP_DIV
+%left OP_MULT
+%left IGUAL
+%left DISTINTO
+%left MAYOR
+%left MAYOR_IGUAL
+%left MENOR
+%left MENOR_IGUAL
+%left OR
+%left AND
+%start programa
+%type <intVal> CTE_INT
+%type <floatVal> CTE_REAL
+%type <strVal> CTE_STRING
+%type <strVal> ID
+%type <auxLogicOperator> logic_operator
+%type <auxLogicOperator> logic_concatenator
+
+%union 
+{
+  int intVal;
+  float floatVal;
+  char strVal[30];
+  char* auxLogicOperator;
+}
+
+%%
+
+
+programa: sentencia_declaracion {printf("\n Regla: COMPILACION EXITOSA\n");}
+  ;
+
+sentencia_declaracion: bloque_declaracion_variables bloque {printf("\n Regla: sentencia: bloque_declaracion_variables bloque \n");}
+  | bloque_declaracion_variables {printf("\n Regla: sentencia: bloque_declaracion_variables \n");}
+  ;
+
+bloque_declaracion_variables: VAR declaracion_variables ENDVAR {printf("\n Regla: bloque_declaracion_variables: VAR declaracion_variables ENDVAR \n");}
+  ;
+
+declaracion_variables: declaracion_variables declaracion_variable {printf("\n Regla: declaracion_variables: declaracion_variables declaracion_variable \n");}
+  | declaracion_variable {printf("\n Regla: declaracion_variables: declaracion_variable \n");}
+
+declaracion_variable: tipo_variable DOS_PUNTOS lista_variables {printf("\n Regla: declaracion_variable: tipo_variable DOS_PUNTOS lista_variables\n");}
+  ;
+tipo_variable: INT {printf("\n Regla: tipo_variable: INT \n");}
+  | FLOAT {printf("\n Regla: tipo_variable: FLOAT \n");}
+  | STRING {printf("\n Regla: tipo_variable: STRING \n");}
+  ;
+lista_variables: lista_variables PUNTO_COMA ID {printf("\n Regla: lista_variables: lista_variables PUNTO_COMA ID \n");} 
+  | ID {printf("\n Regla: lista_variables: ID \n");}
+  ;
+
+bloque: sentencia {printf("\n Regla: bloque: sentencia \n");}
+  | bloque sentencia {printf("\n Regla: bloque: bloque sentencia \n");}
+  ;
+
+sentencia: decision {printf("\n Regla: sentencia: decision \n");}
+  | asignacion {printf("\n Regla: sentencia: asignacion \n");}
+  | print {printf("\n Regla: sentencia: print \n");}
+  | read {printf("\n Regla: sentencia: read \n");}
+  | iteracion {printf("\n Regla: sentencia: iteracion \n");}
+  | asignacion_constante  {printf("Regla: asignacion_constante OK\n");}
+  ;
+
+decision: IF P_A condicion P_C bloque ELSE bloque ENDIF {printf("\n Regla: decision: IF P_A condicion P_C bloque ELSE bloque ENDIF \n");}
+  | IF P_A condicion P_C bloque ENDIF {printf("\n Regla: decision: IF P_A condicion P_C bloque ENDIF \n");}
+  ;
+
+asignacion: ID ASIG expresion {printf("\n Regla: asignacion: ID ASIG expresion \n");};
+
+asignacion_constante: CONST ID ASIG expresion {printf("\n Regla: asignacion_constante: CONST ID ASIG expresion \n");};
+
+iteracion: REPEAT bloque UNTIL P_A condicion P_C {printf("\n Regla: iteracion: REPEAT bloque UNTIL P_A condicion P_C \n");};
+
+condicion_filter: comparacion_filter { printf("\n Regla: condicion_filter: comparacion_filter \n");}
+  | comparacion_filter logic_concatenator comparacion_filter {printf("\n Regla: condicion_filter: comparacion_filter logic_concatenator comparacion_filter \n");}
+  | NOT P_A comparacion_filter P_C {printf("\n Regla: condicion_filter: NOT comparacion_filter \n");}
+  ;
+
+comparacion_filter: GUION_BAJO logic_operator  expresion {printf("\n Regla: comparacion_filter: expresion  logic_operator  expresion \n");}
+  ;
+
+filter: FILTER P_A condicion_filter COMA C_A filterlist C_C P_C {printf("\n Regla: filter: FILTER P_A condicion_filter COMA C_A filterlist C_C P_C \n");}
+  ;
+
+filterlist: filterlist COMA ID {printf("\n Regla: filterlist: filterlist COMA ID \n");}
+      | ID {printf("\n Regla: filterlist: ID \n");}
+      ;
+
+print: PRINT ID {printf("\n Regla: print: PRINT ID \n");}
+  | PRINT CTE_INT {printf("\n Regla: print: PRINT CTE_INT \n");}
+  | PRINT CTE_REAL {printf("\n Regla: print: PRINT CTE_REAL \n");}
+  | PRINT CTE_STRING {printf("\n Regla: print: PRINT CTE_STRING \n");}
+  ;
+
+read: READ ID {printf("\n Regla: read: READ ID \n");};
+
+condicion: comparacion {printf("\n Regla: condicion: comparacion \n");}
+  | comparacion logic_concatenator comparacion {printf("\n Regla: condicion: comparacion logic_concatenator comparacion \n");}
+  | NOT P_A comparacion P_C {printf("\n Regla: condicion: NOT comparacion \n");}
+  ;
+
+comparacion: expresion  logic_operator  expresion {printf("\n Regla: comparacion: expresion  logic_operator  expresion \n");}
+  ;
+
+logic_operator: IGUAL {printf("\n Regla: logic_operator: IGUAL \n");}
+  | DISTINTO {printf("\n Regla: logic_operator: DISTINTO \n");}
+  | MAYOR {printf("\n Regla: logic_operator: MAYOR \n");}
+  | MAYOR_IGUAL {printf("\n Regla: logic_operator: MAYOR_IGUAL \n");}
+  | MENOR {printf("\n Regla: logic_operator: MENOR \n");}
+  | MENOR_IGUAL {printf("\n Regla: logic_operator: MENOR_IGUAL \n");}
+  ;
+
+logic_concatenator: OR {printf("\n Regla: logic_concatenator: OR \n");}
+  | AND {printf("\n Regla: logic_concatenator: AND \n");}
+  ;
+
+expresion: expresion OP_SUMA termino {printf("\n Regla: expresion: expresion OP_SUMA termino\n");}
+  | expresion OP_RESTA termino { printf("\n Regla: expresion: expresion OP_RESTA termino\n");}
+  | termino {printf("\n Regla: expresion: termino\n");}
+  ;
+
+termino: termino OP_MULT factor {printf("\n Regla: termino: termino OP_MULT factor\n");}
+  | termino OP_DIV factor {printf("\n Regla: termino: termino OP_DIV factor\n");}
+  | factor {printf("\n Regla: termino: factor\n");}
+  ;
+
+factor: ID {printf("\n Regla: factor: ID \n");}
+  | CTE_INT {printf("\n Regla: factor: CTE_INT \n");}
+  | CTE_REAL { printf("\n Regla: factor: CTE_REAL \n");}
+  | CTE_STRING {printf("\n Regla: factor: CTE_STRING \n");}
+  | P_A expresion P_C {printf("\n Regla: factor: P_A expresion P_C \n");}
+  | filter { printf("filter OK\n"); }
+  ;
+
+
+%%
+
+int main(int argc, char *argv[]) 
+{
+	yyin = fopen(argv[1], "r");
+  yydebug = 0;
+  printf("COMENZANDO COMPILACION\n");
+  symbolTable = NULL;
+	do 
+  {
+		yyparse();
+	} 
+  while(!feof(yyin));
+  printTable();
+  saveTable();
+	return 0;
+}
+
+
---
+++ symbol.h
@@ -0,0 +1,312 @@
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h> 
+#include <stdio.h>
+
+typedef struct listSymbol {
+    char name[220];
+    char type[220];
+    char value[220];
+    int length;
+    struct listSymbol* next;
+} symbolNode;
+
+
+typedef struct symbolIdentifier {
+    char *value;
+    struct symbolIdentifier* next;
+} identifierNode;
+
+
+
+
+// Symbol table prototypes
+symbolNode* symbolTable;
+symbolNode* insert();
+symbolNode* findSymbol();
+void putTypeIdentifierOnSymbolTable();
+void concatenate();
+void removeChar();
+void printTable();
+void saveTable();
+char* substring();
+char* getSymbolName();
+
+// Symbol Identifier auxiliars
+identifierNode* identifierList;
+identifierNode* insertIdentifier();
+identifierNode* findIdentifier();
+void clearIdentifierList();
+
+
+void displayAlreadyDeclaredErrorFor();
+
+
+symbolNode* insert(char* value) {
+    symbolNode* foundNode = findSymbol(value);
+    if (foundNode != NULL) {
+        return foundNode;
+    }
+    
+    symbolNode* node = (symbolNode*) malloc(sizeof(symbolNode));
+    int len = strlen(value);
+    char* valueToInsert = (char*) malloc(len+1);
+    strcpy(valueToInsert, value);
+    
+    // Is it a string constant?
+    int isConstant = 0;
+    int shouldApplyBase2Transformation = 0;
+    int shouldApplyBase16Transformation = 0;
+    if (valueToInsert[0] == '"') {
+        removeChar(valueToInsert, '"');
+        node->length = strlen(valueToInsert);
+        isConstant = 1;
+        strcpy(node->type, "STRING_C");
+        //node->type = "STRING_C";
+        // Is it a float constant?
+    } else if (strchr(valueToInsert, '.') != NULL) {
+        isConstant = 1;
+        strcpy(node->type, "FLOAT_C");
+
+        //node->type = "FLOAT_C";
+        // Is it a integer constant?
+    } else if (isdigit(valueToInsert[0]) != 0) {
+        isConstant = 1;
+        strcpy(node->type, "INTEGER_C");
+
+        //node->type = "INTEGER_C";
+        if (valueToInsert[0] == '0') {
+            if(valueToInsert[1] == 'b') {
+                shouldApplyBase2Transformation = 1;
+            } else if (valueToInsert[1] == 'x') {
+                shouldApplyBase16Transformation = 1;
+            }
+        }
+        
+    }
+    
+    if (isConstant == 1) {
+        strcpy(node->name, "");
+        node->name[0] = '_' ;
+        concatenate(node->name, valueToInsert);
+    } else {
+        strcpy(node->name, valueToInsert);
+    }
+
+    if(shouldApplyBase2Transformation == 1) {
+        // TODO: Refactor this, extract behaviour into function..
+        char* literalValue = substring(valueToInsert, 3, strlen(valueToInsert));
+        int transformedValue = (int) strtol(literalValue, NULL, 2);
+        itoa(transformedValue, valueToInsert, 10);
+    } else if (shouldApplyBase16Transformation == 1) {
+        char* literalValue = substring(valueToInsert, 3, strlen(valueToInsert));
+        int transformedValue = (int) strtol(literalValue, NULL, 16);
+        itoa(transformedValue, valueToInsert, 10);
+    }
+    if (isConstant == 1) {
+        //node->value = valueToInsert;
+        strcpy(node->value, valueToInsert);
+    }
+
+    node->next = symbolTable;
+    symbolTable = node;
+    return node;
+}
+
+symbolNode* findSymbol(char* value) {
+    symbolNode* tableNode = symbolTable;
+    while(tableNode != NULL){
+        if ((tableNode->value != NULL && strcmp(value, tableNode->value) == 0) || (strcmp(value, tableNode->name) == 0)) {
+            return tableNode;
+        }
+        tableNode = tableNode->next;
+    }
+    return NULL;
+}
+
+
+void printTable() {
+    symbolNode* current = symbolTable;
+    printf("\n TABLA DE SIMBOLOS \n");
+    printf("\nNOMBRE\tTIPODATO\tVALOR\tLONGITUD\n");
+    while(current != NULL){
+        printf("%s\t%s\t%s\t%d\n", current->name, current->type, current->value, current->length);
+        current = current->next;
+    }
+    
+}
+
+
+
+void concatenate(char* original, char* add) {
+  
+  
+  original++;
+   
+     
+   while(*add){
+      *original = *add;
+      add++;
+      original++;
+   }
+   *original = '\0';
+
+}
+
+
+void removeChar(char *s, int c){ 
+  
+    int j, n = strlen(s); 
+    int i;
+    for (i=j=0; i<n; i++) 
+       if (s[i] != c) 
+          s[j++] = s[i]; 
+      
+    s[j] = '\0'; 
+}
+
+
+identifierNode* insertIdentifier(char *name) {
+    identifierNode* foundNode = findIdentifier(name);
+    if (foundNode != NULL) {
+        return foundNode;
+    }
+
+
+    identifierNode* node = (identifierNode*) malloc(sizeof(identifierNode));
+    int len = strlen(name);
+    char* valueToInsert = (char *) malloc(len+1);
+    strcpy(valueToInsert, name);
+
+    node->value = valueToInsert;
+    node->next = identifierList;
+    identifierList = node;
+    return node;
+}
+
+
+
+identifierNode* findIdentifier(char* value) {
+    identifierNode* identifierNode = identifierList;
+    while(identifierNode != NULL) {
+        if (strcmp(value, identifierNode->value) == 0) {
+            return identifierNode;
+        }
+        identifierNode = identifierNode->next;
+    }
+    return NULL;
+}
+
+
+
+void putTypeIdentifierOnSymbolTable(char* type) {
+
+    identifierNode* identifierNode = identifierList;
+    
+    while(identifierNode != NULL) {
+        symbolNode* symbol = findSymbol(identifierNode->value);
+        // Symbol should never be NULL but just in case..
+        if (symbol != NULL) {
+            if (strlen(symbol->type) != 0) {
+                fprintf(stderr, "\n ERROR: Variable %s has been already declared", symbol->name);
+                exit(1);
+            }
+            
+            int len = strlen(type);
+            char* valueToInsert = (char*) malloc(len+1);
+            strcpy(valueToInsert, type);
+        
+            
+            //symbol->type = valueToInsert;
+            strcpy(symbol->type, valueToInsert);
+        }
+        
+        identifierNode = identifierNode->next;
+    }
+    clearIdentifierList();
+}
+
+void clearIdentifierList() {
+    identifierList = NULL;
+}
+
+
+
+char* substring(char *string, int position, int length)
+{
+   char *pointer;
+   int c;
+ 
+   pointer = (char*) malloc(length+1);
+   
+   if (pointer == NULL)
+   {
+      printf("Unable to allocate memory.\n");
+      exit(1);
+   }
+ 
+   for (c = 0 ; c < length ; c++)
+   {
+      *(pointer+c) = *(string+position-1);      
+      string++;  
+   }
+ 
+   *(pointer+c) = '\0';
+ 
+   return pointer;
+}
+
+
+
+
+
+void displayAlreadyDeclaredErrorFor(char* type) {
+    fprintf(stderr, "\n ERROR: Variable type %s was already declared, only one is allowed", type);
+    exit(1);
+}
+
+
+
+void saveTable() {
+    FILE *file = fopen("ts.txt", "w");
+    if (file == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    symbolNode* current = symbolTable;
+    fprintf(file, "NOMBRE\tTIPODATO\tVALOR\tLONGITUD\n");
+    while(current != NULL){
+        fprintf(file, "%s\t%s\t%s\t%d\n", current->name, current->type, current->value, current->length);
+        current = current->next;
+    }
+    fclose(file);
+}
+
+
+char* getSymbolName(void *symbolPointer, int type) {
+    char symbol[220];
+    int integerValue;
+    float floatValue;
+    switch (type) {
+        case 1:
+            integerValue = *(int*)symbolPointer;
+            itoa(integerValue, symbol, 10);
+            break;
+        case 2:
+            floatValue = *(float*)symbolPointer;
+            gcvt (floatValue, 7, symbol);
+            break; 
+        case 3:
+            strcpy(symbol, (char*)symbolPointer);
+            removeChar(symbol, '"');
+    }
+    symbolNode* node = findSymbol(symbol);
+    if (node == NULL) {
+        fprintf(stderr, "\n ERROR: symbol %s not found", symbol);
+        exit(1);
+    }
+
+    return strdup(node->name);
+    
+}
